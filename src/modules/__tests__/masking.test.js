7d9f540202a364d97b9cae42b1141d0614c10fe7
Jesse Myers <jesse.myers@globality.com>
2018-05-04 12:48:55 -0700

    Better masking functions

---
+++ src/modules/__tests__/masking.test.js
@@ -0,0 +1,13 @@
+import { withArgs } from 'index';
+
+
+describe('withArgs', () => {
+
+    it('replaces arguments', () => {
+        expect(withArgs(0)(1, 2, 3, 4)).toEqual([1, 0, 3, 4]);
+    });
+
+    it('replaces arguments with a function', () => {
+        expect(withArgs(obj => 8 * obj)(1, 2, 3, 4)).toEqual([1, 8, 3, 4]);
+    });
+});
