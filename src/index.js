befb304a9d30aba3524c688882faebcb23555634
Tom Caspy <tcaspy@gmail.com>
2018-09-12 17:11:39 +0300

    exposing the named function to imports (#68)
    Why?
    when using the batched wrapper, we need to use the named function for building
    the batching configuration

--- src/index.js
+++ src/index.js
@@ -19,4 +19,4 @@ export {
     signPrivate,
 } from './testing';
 export { wrapResolvers } from './wrapper';
-export { default as bindServices } from './services';
+export { default as bindServices, named } from './services';
