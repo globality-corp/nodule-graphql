627f6b7ca32ff0d29c06e4247da251dcf1ee47ce
Amby Balaji <ambareesh.balaji@globality.com>
2018-11-09 16:23:30 -0800

    Create createGraphQLEnumType utility

---
+++ src/enums/index.js
@@ -0,0 +1,14 @@
+/* eslint-disable import/prefer-default-export */
+
+import { GraphQLEnumType } from 'graphql';
+
+// converts an Enum type created using the 'enum' package to a GraphQLEnumType
+export const createGraphQLEnumType = (standardEnum, name, description = '') =>
+    new GraphQLEnumType({
+        name,
+        description,
+        values: standardEnum.enums.reduce((acc, enum_) => {
+            acc[enum_.key] = { value: enum_.value };
+            return acc;
+        }, {}),
+    });
