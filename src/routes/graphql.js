416ad902eb0cab904245ea8c4eaeeda5abab103c
Michael Cohen <michael.lee.cohen@gmail.com>
2021-10-25 10:08:45 -0700

    GLOB-58608: Enhance `routes.graphql.apolloPlugins`config binding (#142)
    Updated `routes.graphql.apolloPluginsconfig` binding to bind by key
    
    before
    
    ```
    setDefaults('routes.graphql.apolloPlugins', [pluginOne, pluginTwo]);
    ```
    
    after
    
    ```
    setDefaults('routes.graphql.apolloPlugins.pluginOne', pluginOne);
    setDefaults('routes.graphql.apolloPlugins.pluginTwo', pluginTwo);
    ```

--- src/routes/graphql.js
+++ src/routes/graphql.js
@@ -129,6 +129,9 @@ function createApolloServerOptions() {
     }
 
     const { apolloEngine, apolloPlugins } = config.routes.graphql;
+    const plugins = apolloPlugins
+        ? Object.keys(apolloPlugins).map((key) => apolloPlugins[key])
+        : [];
 
     const {
         enabled: engineEnabled,
@@ -143,7 +146,7 @@ function createApolloServerOptions() {
         rootValue: null,
         schema,
         engine: engineEnabled ? engineConfig : false,
-        plugins: apolloPlugins || [],
+        plugins,
     };
 }
 
