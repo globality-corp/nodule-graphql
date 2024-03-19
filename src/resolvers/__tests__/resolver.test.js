5f7d45370bfe609ee617b2843decd2608a800a03
Łukasz German <52422345+german-glob@users.noreply.github.com>
2024-03-19 17:03:40 +0100

    Added overfetching guard to resolver class (#175)

--- src/resolvers/__tests__/resolver.test.js
+++ src/resolvers/__tests__/resolver.test.js
@@ -76,4 +76,54 @@ describe('a resolver', () => {
         });
         expect(await resolver.resolve()).toEqual(42);
     });
+
+    it('maxCallsPerRequest not used when context not passed', async () => {
+        const resolver = createResolver({
+            aggregate: async () => 42,
+            mask: () => [],
+            maxCallsPerRequest: 0,
+        });
+        expect(await resolver.resolve()).toEqual(42);
+    });
+
+    it('maxCallsPerRequest not used when not directly enabled', async () => {
+        const context = {};
+        const resolver = createResolver({
+            aggregate: async () => 42,
+            mask: () => [],
+        });
+        expect(await resolver.resolve(undefined, undefined, context)).toEqual(42);
+        expect(context.callCounts).toBeUndefined();
+    });
+
+    it('maxCallsPerRequest sets call counts in the context using resolver id', async () => {
+        const context = {};
+        const resolverA = createResolver({
+            aggregate: async () => 42,
+            mask: () => [],
+            maxCallsPerRequest: 1,
+        });
+        const resolverB = createResolver({
+            aggregate: async () => 34,
+            mask: () => [],
+            maxCallsPerRequest: 1,
+        });
+        expect(await resolverA.resolve(undefined, undefined, context)).toEqual(42);
+        expect(await resolverB.resolve(undefined, undefined, context)).toEqual(34);
+        expect(context.callCounts[resolverA.resolverId]).toEqual(1);
+        expect(context.callCounts[resolverB.resolverId]).toEqual(1);
+        expect(resolverA.resolverId === resolverB.resolverId).toBeFalsy();
+    });
+
+    it('maxCallsPerRequest triggers error when exceeded', async () => {
+        const context = {};
+        const resolver = createResolver({
+            aggregate: async () => 42,
+            mask: () => [],
+            maxCallsPerRequest: 1,
+        });
+        expect(await resolver.resolve(undefined, undefined, context)).toEqual(42);
+        expect(await resolver.resolve(undefined, undefined, context)).toEqual(42);
+        expect(context.callCounts[resolver.resolverId]).toEqual(2);
+    });
 });
--- src/resolvers/types.js
+++ src/resolvers/types.js
@@ -1,5 +1,6 @@
 import { getContainer } from '@globality/nodule-config';
 import { isFunction, isNil } from 'lodash';
+import { v4 as uuidv4 } from 'uuid';
 
 /**
  * Default mask function: preserves standard arguments
@@ -8,35 +9,55 @@ function defaultMask(obj, args, context, info) {
     return [obj, args, context, info];
 }
 
-/**
- * A resolver is a structured abstraction around a GraphQL resolver function.
- *
- * It includes:
- *
- *  -  An async `aggregate` function, which queries/mutates data from one or more services.
- *  -  An async `authorize` function, which throws an error if the resolver should not run.
- *  -  A user-defined `authorizedData` payload, which is passed to the `authorize` function
- *     (if provided).
- *  -  A synchronous `transform` function, which presents the result of the aggregation in
- *     the expected shape of the upstream resource.
- *  -  A synchronous `mask` function, which manipulates the standard GraphQL resolver
- *     arguments into a form supported by the other functions.
- *  -  An async `preAggregate` function, which can be used to factor out some preliminary work,
- *     that requires a network call. It can alter the arguments passed to the `aggregate` function,
- *     but can't change their order. Note it gets arguments in the order they are returned by `mask`.
- */
 export class Resolver {
-    constructor({ aggregate, authorize, authorizeData, transform, mask, preAggregate }) {
+    /**
+     * A resolver is a structured abstraction around a GraphQL resolver function.
+     *
+     * @param {Object} options
+     * @param {Function} options.aggregate An async function, which queries/mutates data from one or more services.
+     * @param {Function} options.authorize An async function, which throws an error if the resolver should not run.
+     * @param {Function} options.authorizeData A user-defined payload, which is passed to the `authorize` function
+     * (if provided).
+     * @param {Function} options.transform A synchronous function, which presents the result of the aggregation in
+     * the expected shape of the upstream resource.
+     * @param {Function} options.mask A synchronous function, which manipulates the standard GraphQL resolver arguments
+     * into a form supported by the other functions.
+     * @param {Function} options.preAggregate An async function, which can be used to factor out some preliminary work,
+     * that requires a network call. It can alter the arguments passed to the `aggregate` function, but can't change their order.
+     * Note it gets arguments in the order they are returned by `mask`.
+     * @param {Number} options.maxCallsPerRequest An integer, which specifies the number of times the resolver can be called
+     * within a single request. More calls will rsult in logging a warning, that can be used for alerting. This is to prevent
+     * overfetching by API clients, when it's known that a given resolver should be triggered only once, for a single parent object.
+     */
+    constructor({ aggregate, authorize, authorizeData, transform, mask, preAggregate, maxCallsPerRequest }) {
         this.aggregate = aggregate;
         this.authorize = authorize;
         this.authorizeData = authorizeData;
         this.transform = transform;
         this.mask = mask || defaultMask;
         this.preAggregate = preAggregate || null;
+        this.maxCallsPerRequest = maxCallsPerRequest || null;
+        this.resolverId = uuidv4();
     }
 
     // NB: async class methods were added to node in v8.x
     async resolve(obj, args, context, info) {
+        // enforce overfetching guard
+        if (this.maxCallsPerRequest && !isNil(context)) {
+            if (isNil(context.callCounts)) {
+                context.callCounts = {};
+            }
+            if (isNil(context.callCounts[this.resolverId])) {
+                context.callCounts[this.resolverId] = 0;
+            }
+            const callCount = ++context.callCounts[this.resolverId]; // eslint-disable-line no-plusplus
+            if (callCount > this.maxCallsPerRequest) {
+                const { logger } = getContainer();
+                const msg = `Exceeded maximum number of resolver calls per request: ${this.maxCallsPerRequest}, called ${callCount} times.`;
+                logger.warning(context, msg);
+            }
+        }
+
         if (this.authorize) {
             // allow authorizer to be looked up by name
             const authorize = isFunction(this.authorize) ? this.authorize : getContainer(`graphql.authorizers.${this.authorize}`);
