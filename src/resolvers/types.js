1058ae9ee25b3916e56ab13c6e6cf7b2ba4993a6
William Parsons-Heins <william.parsons-heins@globality.com>
2024-04-22 15:17:19 +0100

    allow createResolver to accept a generic for the shape of the aggregate

--- src/resolvers/types.js
+++ src/resolvers/types.js
@@ -3,31 +3,38 @@ import { isFunction, isNil } from 'lodash';
 import { v4 as uuidv4 } from 'uuid';
 
 /**
+ * A resolver is a structured abstraction around a GraphQL resolver function.
+ *
+ * @template T
+ * @typedef {Object} Options
+ * @prop {T} aggregate An async function, which queries/mutates data from one or more services.
+ * @prop {Function=} authorize An async function, which throws an error if the resolver should not run.
+ * @prop {Function=} authorizeData A user-defined payload, which is passed to the `authorize` function
+ * (if provided).
+ * @prop {Function=} transform A synchronous function, which presents the result of the aggregation in
+ * the expected shape of the upstream resource.
+ * @prop {Function=} mask A synchronous function, which manipulates the standard GraphQL resolver arguments
+ * into a form supported by the other functions.
+ * @prop {Function=} preAggregate An async function, which can be used to factor out some preliminary work,
+ * that requires a network call. It can alter the arguments passed to the `aggregate` function, but can't change their order.
+ * Note it gets arguments in the order they are returned by `mask`.
+ * @prop {Number=} maxCallsPerRequest An integer, which specifies the number of times the resolver can be called
+ * within a single request. More calls will rsult in logging a warning, that can be used for alerting. This is to prevent
+ * overfetching by API clients, when it's known that a given resolver should be triggered only once, for a single parent object.
+ */
+
+/**
  * Default mask function: preserves standard arguments
  */
 function defaultMask(obj, args, context, info) {
     return [obj, args, context, info];
 }
 
+/** @template T */
 export class Resolver {
     /**
-     * A resolver is a structured abstraction around a GraphQL resolver function.
-     *
-     * @param {Object} options
-     * @param {Function} options.aggregate An async function, which queries/mutates data from one or more services.
-     * @param {Function} options.authorize An async function, which throws an error if the resolver should not run.
-     * @param {Function} options.authorizeData A user-defined payload, which is passed to the `authorize` function
-     * (if provided).
-     * @param {Function} options.transform A synchronous function, which presents the result of the aggregation in
-     * the expected shape of the upstream resource.
-     * @param {Function} options.mask A synchronous function, which manipulates the standard GraphQL resolver arguments
-     * into a form supported by the other functions.
-     * @param {Function} options.preAggregate An async function, which can be used to factor out some preliminary work,
-     * that requires a network call. It can alter the arguments passed to the `aggregate` function, but can't change their order.
-     * Note it gets arguments in the order they are returned by `mask`.
-     * @param {Number} options.maxCallsPerRequest An integer, which specifies the number of times the resolver can be called
-     * within a single request. More calls will rsult in logging a warning, that can be used for alerting. This is to prevent
-     * overfetching by API clients, when it's known that a given resolver should be triggered only once, for a single parent object.
+     * @constructor
+     * @param {Options<T>}
      */
     constructor({ aggregate, authorize, authorizeData, transform, mask, preAggregate, maxCallsPerRequest }) {
         this.aggregate = aggregate;
@@ -92,6 +99,8 @@ export class Resolver {
 
 /**
  * Create a resolver.
+ * @template T
+ * @param {Options<T>} options
  */
 export function createResolver(options) {
     return new Resolver(options);
@@ -101,6 +110,8 @@ export function createResolver(options) {
  * Create a strict resolver.
  *
  * A strict resolver must define an authorize and a mask function.
+ * @template T
+ * @param {Options<T>} options
  */
 export function createStrictResolver(options) {
     if (!options.aggregate) {
