487fdd176b12afd1ead652534c7c94b71ccbde58
William Parsons-Heins <142901870+wparsons-heins-globality@users.noreply.github.com>
2024-08-09 15:14:19 +0100

    Better typing (#194)

--- src/resolvers/types.js
+++ src/resolvers/types.js
@@ -2,10 +2,38 @@ import { getContainer } from '@globality/nodule-config';
 import { isFunction, isNil } from 'lodash';
 import { v4 as uuidv4 } from 'uuid';
 
-import { requestContext } from './context'
+import { requestContext } from './context';
+
+/**
+ * @typedef {object} Context
+ * @prop {Record<string, number>} callCounts
+ */
+
+/**
+ * @typedef {object} Options
+ * @prop {(...args: any[]) => Promise<unknown>} aggregate An async function, which queries/mutates data from one or more services.
+ * @prop {(((...args: any[]) => Promise<unknown>) | string)=} authorize An async function, which throws an error if the resolver should not run.
+ * @prop {unknown=} authorizeData A user-defined payload, which is passed to the `authorize` function
+ * (if provided).
+ * @prop {((...args: any[]) => unknown)=} transform A synchronous function, which presents the result of the aggregation in
+ * the expected shape of the upstream resource.
+ * @prop {((...args: any[]) => unknown)=} mask A synchronous function, which manipulates the standard GraphQL resolver arguments
+ * into a form supported by the other functions.
+ * @prop {((...args: any[]) => Promise<unknown>)=} preAggregate An async function, which can be used to factor out some preliminary work,
+ * that requires a network call. It can alter the arguments passed to the `aggregate` function, but can't change their order.
+ * Note it gets arguments in the order they are returned by `mask`.
+ * @prop {number=} maxCallsPerRequest An integer, which specifies the number of times the resolver can be called
+ * within a single request. More calls will result in logging a warning, that can be used for alerting. This is to prevent
+ * over-fetching by API clients, when it's known that a given resolver should be triggered only once, for a single parent object.
+ */
 
 /**
  * Default mask function: preserves standard arguments
+ * @param {unknown} obj
+ * @param {unknown} args
+ * @param {Context} context
+ * @param {unknown} info
+ * @returns {[unknown, unknown, Context, unknown]}
  */
 function defaultMask(obj, args, context, info) {
     return [obj, args, context, info];
@@ -15,21 +43,7 @@ export class Resolver {
     /**
      * A resolver is a structured abstraction around a GraphQL resolver function.
      *
-     * @param {Object} options
-     * @param {Function} options.aggregate An async function, which queries/mutates data from one or more services.
-     * @param {Function} options.authorize An async function, which throws an error if the resolver should not run.
-     * @param {Function} options.authorizeData A user-defined payload, which is passed to the `authorize` function
-     * (if provided).
-     * @param {Function} options.transform A synchronous function, which presents the result of the aggregation in
-     * the expected shape of the upstream resource.
-     * @param {Function} options.mask A synchronous function, which manipulates the standard GraphQL resolver arguments
-     * into a form supported by the other functions.
-     * @param {Function} options.preAggregate An async function, which can be used to factor out some preliminary work,
-     * that requires a network call. It can alter the arguments passed to the `aggregate` function, but can't change their order.
-     * Note it gets arguments in the order they are returned by `mask`.
-     * @param {Number} options.maxCallsPerRequest An integer, which specifies the number of times the resolver can be called
-     * within a single request. More calls will rsult in logging a warning, that can be used for alerting. This is to prevent
-     * overfetching by API clients, when it's known that a given resolver should be triggered only once, for a single parent object.
+     * @param {Options} options
      */
     constructor({ aggregate, authorize, authorizeData, transform, mask, preAggregate, maxCallsPerRequest }) {
         this.aggregate = aggregate;
@@ -42,9 +56,14 @@ export class Resolver {
         this.resolverId = uuidv4();
     }
 
-    // NB: async class methods were added to node in v8.x
+    /**
+     * @param {unknown} obj
+     * @param {unknown} args
+     * @param {Context} context
+     * @param {unknown} info
+     */
     async resolve(obj, args, context, info) {
-        // enforce overfetching guard
+        // enforce over-fetching guard
         if (this.maxCallsPerRequest && !isNil(context)) {
             if (isNil(context.callCounts)) {
                 context.callCounts = {};
@@ -93,6 +112,8 @@ export class Resolver {
 
 /**
  * Create a resolver.
+ * @param {Options} options
+ * @returns {Resolver}
  */
 export function createResolver(options) {
     return new Resolver(options);
@@ -102,6 +123,8 @@ export function createResolver(options) {
  * Create a strict resolver.
  *
  * A strict resolver must define an authorize and a mask function.
+ * @param {Options} options
+ * @deprecated At a type-level we require an aggregate and authorize anyway, prefer `createResolver`.
  */
 export function createStrictResolver(options) {
     if (!options.aggregate) {
